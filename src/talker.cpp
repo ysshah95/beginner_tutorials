83382a81ebf508baafa27399c15a778194a13354
Yash Shah <ysshah95@umd.edu>
2018-10-29 16:45:24 -0400

    Updated src/talker.cpp and src/listener.cpp to remove cpplint errors. Also added Copyright message in those files.

--- src/talker.cpp
+++ src/talker.cpp
@@ -1,7 +1,42 @@
-#include "ros/ros.h"
-#include "std_msgs/String.h"
+/**
+ * MIT License
+ * 
+ * Copyright (c) 2018 Yash Shah
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/**
+ * @file talker.cpp
+ * @Auther Yash Shah
+ * @version 1.0
+ * @brief This is a ROS Node that publishes to the topic "chatter"
+ * 
+ * This node implementation publishes messages to the topic "chatter" and 
+ * keeps count of number of messages. 
+ * 
+ * @copyright MIT License (c) 2018  
+ */
 
 #include <sstream>
+
+#include "ros/ros.h"
+#include "std_msgs/String.h"
 
 /**
  * This tutorial demonstrates simple sending of messages over the ROS system.
